63d11f6d694a9198f9c0c9fc92c73605f799d38f
HARU-pc <4538haru@gmail.com>
2023-04-22 08:10:49 +0900

    Update main.py

---
+++ main.py
@@ -0,0 +1,78 @@
+from module import Aes,Save_Date
+import re
+import os
+import sys
+import subprocess
+import platform
+import hashlib
+import dill
+from getpass import getpass
+import blackjack
+
+
+class Main:
+
+    def __init__(self) -> None:
+
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+        print("♥♦Welcome!!♠♣\nLet's enjoy\n")
+
+    def Create_new_user(self):
+
+        self.Game_Data = Save_Date.Data.Creat(Game_Name)
+        if self.Game_Data == None:
+            self.New_User_or_Load_Data()
+        else:
+            Save_Date.Data.Save(self.Game_Data,Game_Name)
+            self.Select_Game()
+
+    def Load_Data(self):
+
+        self.Game_Data = Save_Date.Data.Load(Game_Name)
+        if self.Game_Data == None:
+            self.New_User_or_Load_Data()
+        else:
+            self.Select_Game()
+
+    def New_User_or_Load_Data(self):
+
+        print('\nCreate new user:1\nLoad save data:2')
+        New_or_Load = input('Input field:')
+        while re.search(r'[.*?new.*?|.*?create.*?|.*?load.*?|1|2]',New_or_Load.lower()) == None:
+            print(f'\nERROR:There is no {New_or_Load} in the choices.\nCreate new Game:1\nLoad save data:2 ')
+            New_or_Load = input('Input field:')
+
+        if re.search(r'[.*?new.*?|.*?create.*?|1]',New_or_Load.lower()) != None:
+            self.Create_new_user()
+        elif re.search(r'[.*?load.*?|2]',New_or_Load.lower()) != None:
+            self.Load_Data()
+
+    def Select_Game(self):
+
+        Games = ["Blackjack"]
+        Game_List_Number = 0
+        print("\nGame List\n")
+
+        for Game_List in Games:
+            Game_List_Number += 1
+            print(Game_List_Number,":",Game_List,"\n")
+
+        Chosen_Game = input("\nWhich game do you like?  ")
+
+        while Chosen_Game != '1':
+            print("Please select a game by number.")
+            Chosen_Game = input("\nWhich game do you like?  ")
+
+        if Chosen_Game == '1':
+            BlackJack = blackjack.App(self.Game_Data.name,self.Game_Data.passwd)
+            BlackJack.main()
+#try:
+Game_Name = 'Home'
+Home = Main()
+Home.New_User_or_Load_Data()
+Home.Select_Game()
+
+#except (KeyboardInterrupt, BaseException):
+#    print("\n\nThank you for playing!!")
+#    pass
